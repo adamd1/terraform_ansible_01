77b440154ada38524154a5dadc93d1e673832203
Adam Drake <adamd1@users.noreply.github.com>
2016-01-22 14:25:11 -0500

    Create setupansible.sh
    A shell script which in isolation will update the local ubuntu server and then provision it with ansible.

---
+++ setupansible.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install linux-headers-$(uname -r)
+sudo apt-get -y install dkms
+sudo apt-get -y install python2.7-dev python2.7 python-pip libyaml-dev libssl-dev libreadline-gplv2-dev
+sudo pip install ansible
+sudo apt-get -y install unzip
